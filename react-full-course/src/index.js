a382bbac95a8b74eaa8f40edce2ea4da170c422e
mcpadma <jmcpadma@gmail.com>
2020-08-09 19:08:13 +0530

    sample class based component rendering

--- react-full-course/src/index.js
+++ react-full-course/src/index.js
@@ -4,12 +4,25 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+class Hello extends React.Component {
+  render() {
+    return(
+      <h1> Welcome to React</h1>
+    )
+  }
+}
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Hello />
   </React.StrictMode>,
   document.getElementById('root')
 );
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
